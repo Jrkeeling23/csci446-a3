fe3fe0562915702ba3d2f00e9fd563db3037c2fe
jrkeeling23 <jrkeeling23@gmail.com>
2018-11-30 09:52:49 -0700

    square updated with single constructor and hard coded array of enviroment attributes are set to false.

--- src/wumpasWorld/Square.java
+++ src/wumpasWorld/Square.java
@@ -4,7 +4,7 @@ package wumpasWorld;
  * A Square instance is created for each square in the game. It holds environmental variables that an agent will use to
  * detect the state of that particular square.
  */
-public class Square{
+public class Square {
 
     // variable to hold x,y cordinates of square
     public int row, col;
@@ -17,35 +17,40 @@ public class Square{
      * 3 : wumpus
      * 4 : pit
      */
-    private boolean environment_attributes[];
+    private boolean []environment_attributes = new boolean[5];
 
-	public Square(int row, int col) {
-		// TODO Auto-generated constructor stub
-	}
+    public Square(int row, int col) {
+        this.row = row;
+        this.col = col;
+        for(int i = 0; i < environment_attributes.length; i++){
+            environment_attributes[i] = false;
+        }
 
-	public Square(int row, int col, boolean[] environment_attributes) {
-		this.row = row;
-		this.col = col;
-		this.environment_attributes = environment_attributes;
-	}
+    }
 
-	public boolean has_pit() {
-		if(environment_attributes[4] == true){
-			return true;
-		}
-		return false;
-	}
+//    public Square(int row, int col, boolean[] environment_attributes) {
+//        this.row = row;
+//        this.col = col;
+//        this.environment_attributes = environment_attributes;
+//    }
 
-	public void add_obj(EnvType env) {
-		int index = env.ordinal();
-		environment_attributes[index] = true;
+    public boolean has_pit() {
+        if (environment_attributes[4] == true) {
+            return true;
+        }
+        return false;
+    }
+
+    public void add_obj(EnvType env) {
+        int index = env.ordinal();
+        environment_attributes[index] = true;
 
 
-	}
+    }
 
-	public void remove_obj(EnvType env) {
-		int index = env.ordinal();
-		environment_attributes[index] = false;
-		
-	}
+    public void remove_obj(EnvType env) {
+        int index = env.ordinal();
+        environment_attributes[index] = false;
+
+    }
 }
